3029add8effb042d94acb81a85d5699c45210b83
Vladislav <vlad.bel.kos@mail.ru>
2022-09-04 15:11:18 +0300

    CSV-JSON

---
+++ Maven_CSV_JSON/src/main/java/Main.java
@@ -0,0 +1,57 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "data.csv";
+
+        List<Employee> list = parseCSV(columnMapping, fileName);
+        String json = listToJson(list);
+        writeString("JsonFile.json", json);
+    }
+
+    private static List<Employee> parseCSV(String[] columnMapping, String fileName) throws FileNotFoundException {
+        List<Employee> staff = null;
+        try (CSVReader csvReader = new CSVReader(new FileReader(fileName))) {
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping(columnMapping);
+            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(csvReader)
+                    .withMappingStrategy(strategy)
+                    .build();
+            staff = csv.parse();
+            staff.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return staff;
+    }
+
+    private static String listToJson(List<Employee> list) {
+        Type listType = new TypeToken<List<Employee>>() {}.getType();
+        Gson gson = new GsonBuilder()
+                .create();
+        String json = gson.toJson(list, listType);
+        return json;
+    }
+
+    private static void writeString(String fileName, String jsonFile) throws IOException {
+        FileWriter fileWriter = new FileWriter(fileName);
+        fileWriter.write(jsonFile);
+        fileWriter.close();
+    }
+}
